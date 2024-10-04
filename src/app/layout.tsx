fdd679415f9c6cbf80aa3b81df060da277066120
Gareth Thomas <11306782+forevergareth@users.noreply.github.com>
2024-10-03 22:43:05 -0400

    test plausible provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import PlausibleProvider from "next-plausible";
 import "./globals.css";
 
 const fontSans = FontSans({
@@ -55,18 +56,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-<script defer data-domain="forevergareth.com" src="http://plausible.178.156.141.108.sslip.io/js/script.outbound-links.js"></script>      <body
+      <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
           fontSans.variable
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <TooltipProvider delayDuration={0}>
-            {children}
-            <Navbar />
-          </TooltipProvider>
-        </ThemeProvider>
+        <PlausibleProvider domain="forevergareth.com" customDomain="https://plausible.kulchaone.com">
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <TooltipProvider delayDuration={0}>
+              {children}
+              <Navbar />
+            </TooltipProvider>
+          </ThemeProvider>
+        </PlausibleProvider>
       </body>
     </html>
   );
