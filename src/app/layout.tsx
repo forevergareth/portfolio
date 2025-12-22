f1693a0212f4e8c53650cc3402e29a7c5ae88033
Gareth Thomas <11306782+forevergareth@users.noreply.github.com>
2025-12-21 19:11:24 -0500

    chore: add @vercel/analytics package and integrate analytics component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import PlausibleProvider from "next-plausible";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const fontSans = FontSans({
@@ -62,7 +63,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <PlausibleProvider domain="forevergareth.com" customDomain="https://plausible.kulchaone.com">
+        <PlausibleProvider domain="forevergareth.com">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             <TooltipProvider delayDuration={0}>
               {children}
@@ -70,6 +71,7 @@ export default function RootLayout({
             </TooltipProvider>
           </ThemeProvider>
         </PlausibleProvider>
+        <Analytics />
       </body>
     </html>
   );
